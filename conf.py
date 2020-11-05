9c9bdd6e5f9bcf4e2a8e2ba1ce8f793beae11899
Robbie Ferguson <robbie@category5.tv>
2020-11-05 14:54:02 -0500

    More config.

--- conf.py
+++ conf.py
@@ -1,3 +1,10 @@
+project = 'NEMS Linux Documentation'
+copyright = '2020, Category5 TV Network'
+author = 'Robbie Ferguson'
+
+version = '1.6'
+release = '1.6'
+
 html_css_files = [
     'css/custom.css',
 ]
